00186b656fbf588c00bafc0f161d71fbc736b8ab
Elsti97 <elsti.shehati@hotmail.it>
2022-10-18 14:24:44 +0200

    Sistemato errori di battitura

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,13 @@
-let NomeUtente = prompt("Inserisci nome")
-console.log(NomeUtente)
+let NomeUtente = prompt("Inserisci il tuo nome")
+console.log(NomeUtente);
 
-let CognomeUtente = prompt("Inserisci cognome")
-console.log(CognomeUtente)
+let CognomeUtente = prompt("Inserisci il tuo cognome")
+console.log(CognomeUtente);
 
 let ColorePreferito = prompt("Inserisci il tuo colore preferito")
-console.log(ColorePreferito)
+console.log(ColorePreferito);
 
 const  NumeroFisso = 21;
 
 document.getElementById('BestPassword').innerHTML = `
-${NomeUtente}${CognomeUtente}${ColorePreferito}${NumeroFisso}`
+${NomeUtente}${CognomeUtente}${ColorePreferito}${NumeroFisso}`;
